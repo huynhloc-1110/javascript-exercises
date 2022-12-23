1651d039656e57655680bac8838234b52394d8ed
huynhloc-1110 <101441531+huynhloc-1110@users.noreply.github.com>
2022-12-23 07:33:24 +0700

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
-
+/**
+ * @param {Array} array
+ */
+const removeFromArray = function(array, ...targets) {
+  for (const target of targets) {
+    let targetIndex = -1;
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] === target) {
+        targetIndex = i;
+        break;
+      }
+    }
+    if (targetIndex == -1) continue;
+    array = array.slice(0, targetIndex).concat(array.slice(targetIndex+1));
+  }
+  return array;
 };
 
 // Do not edit below this line
