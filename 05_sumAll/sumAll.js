570950440fe1790284e9b0e4322a23016cbc86a0
huynhloc-1110 <101441531+huynhloc-1110@users.noreply.github.com>
2022-12-23 07:57:37 +0700

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+/**
+ * @param {Number} num1
+ * @param {Number} num2
+ */
+const sumAll = function(num1, num2) {
+  if (!(typeof num1 == 'number') || !(typeof num2 == 'number')
+    || num1 < 0 || num2 < 0)
+    return "ERROR";
 
+  if (num1 > num2) {
+    [num1, num2] = [num2, num1];
+  }
+
+  let result = 0;
+  for (let i = num1; i <= num2; i++) {
+    result += i;
+  }
+  return result;
 };
 
 // Do not edit below this line
