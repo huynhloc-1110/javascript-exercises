5589005532afa8155838696b982842cb4a99b585
huynhloc-1110 <101441531+huynhloc-1110@users.noreply.github.com>
2022-12-23 07:06:43 +0700

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let stringArr = string.split("");
+  let result = "";
+  for (let i = stringArr.length - 1; i >= 0; i--) {
+    result += stringArr[i];
+  }
+  return result;
 };
 
 // Do not edit below this line
