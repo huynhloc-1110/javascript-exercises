2bf5e3965ef0571a33e70c617e356ed72cf8d281
huynhloc-1110 <101441531+huynhloc-1110@users.noreply.github.com>
2022-12-31 20:23:34 +0700

    Complete Exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, num) => total += num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((result, num) => result *= num, 1);
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  let result = 1;
+	for (let i = 0; i < y; i++) {
+    result *= x;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  if (x < 0) return;
+	if (x <= 1) return 1;
+  let result = 1;
+  for (let i = 2; i <= x; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
