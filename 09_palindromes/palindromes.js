e04374e2dd571787dc1cd3cf339472c0dbf7b423
huynhloc-1110 <101441531+huynhloc-1110@users.noreply.github.com>
2022-12-31 20:40:17 +0700

    Complete Exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
-
+/**
+ * 
+ * @param {string} str 
+ */
+const palindromes = function (str) {
+  let cleanedStr = str
+    .toLowerCase()
+    .split('')
+    .filter(char => char <= 'z' && char >= 'a')
+    .join('');
+  for (let i = 0; i < cleanedStr.length / 2; i++) {
+    if (cleanedStr.charAt(i) != cleanedStr.charAt(cleanedStr.length-i-1)) {
+      return false;
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line
