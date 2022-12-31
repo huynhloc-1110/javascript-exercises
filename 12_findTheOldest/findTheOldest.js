e52749645fc76537aa106663b7453f76cd4fd757
huynhloc-1110 <101441531+huynhloc-1110@users.noreply.github.com>
2022-12-31 21:20:12 +0700

    Complete Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  return people.reduce((oldest, person) => {
+    if (getAge(person) > getAge(oldest)) {
+      oldest = person;
+    }
+    return oldest;
+  }, people[0]);
 };
+
+function getAge(person) {
+  if ('yearOfDeath' in person) {
+    return person.yearOfDeath - person.yearOfBirth;
+  }
+  return (new Date()).getFullYear() - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
