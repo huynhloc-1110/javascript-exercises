6a345e81e700e185cf824428a6861d3db190e029
huynhloc-1110 <101441531+huynhloc-1110@users.noreply.github.com>
2022-12-31 20:49:36 +0700

    Complete Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  if (n <= 0) return "OOPS";
+  let f1 = 1;
+  let f2 = 1;
+  for (let i = 3; i <= n; i++) {
+    [f1, f2] = [f2, f1 + f2];
+  }
+  return f2;
 };
 
 // Do not edit below this line
